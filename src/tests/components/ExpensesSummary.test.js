c0efef3442afd0ee4ce55ef70d342eeed6c9842e
Peter Higgins <pah@cenabo.com>
2018-02-06 14:36:59 +0000

    Add expense summary to dashboard

---
+++ src/tests/components/ExpensesSummary.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expensesSummary, ExpensesSummary } from '../../components/ExpensesSummary';
+import numeral from 'numeral';
+
+test('should correctly render ExpensesSummary with one expense', () => {
+  const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={235} />);
+  expect(wrapper).toMatchSnapShot();
+});
+
+test('should correctly render ExpensesSummary with multiple expenses', () => {
+  const wrapper = shallow(<ExpensesSummary expenseCount={23} expensesTotal={2537287435} />);
+  expect(wrapper).toMatchSnapShot();
+});
